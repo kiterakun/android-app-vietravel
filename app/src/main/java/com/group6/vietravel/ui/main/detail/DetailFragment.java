26c7e05f3bd7861270367bb893353b422cd1c5ff
23120341 <23120341@student.hcmus.edu.com>
2025-10-28 18:17:36 +0700

    setup Android project structure

---
+++ app/src/main/java/com/group6/vietravel/ui/main/detail/DetailFragment.java
@@ -0,0 +1,38 @@
+package com.group6.vietravel.ui.main.detail;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group6.vietravel.R;
+
+public class DetailFragment extends Fragment {
+
+    private DetailViewModel mViewModel;
+
+    public static DetailFragment newInstance() {
+        return new DetailFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DetailViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/group6/vietravel/ui/main/detail/DetailViewModel.java
@@ -0,0 +1,7 @@
+package com.group6.vietravel.ui.main.detail;
+
+import androidx.lifecycle.ViewModel;
+
+public class DetailViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
